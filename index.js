0aeec49118b3df714233b9973d79a56561ca481e
morber <4497491+morber11@users.noreply.github.com>
2024-02-23 23:59:19 +0000

    initial commit for chrome

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-// at some point this will be moved to settings...
 const archiveUrl = 'https://archive.is/?run=1&url='
 
-browser.browserAction.onClicked.addListener((tab) => {
+chrome.action.onClicked.addListener((tab) => {
     let url = `${archiveUrl}${encodeURIComponent(tab.url)}`;
-    browser.tabs.create({ url: url });
+    chrome.tabs.create({ url: url });
 });
 
-browser.contextMenus.create({
+chrome.contextMenus.create({
     id: "archivePage",
     title: "Archive this page",
     contexts: ["page"]
 });
 
-browser.contextMenus.create({
+chrome.contextMenus.create({
     id: "archiveLink",
     title: "Archive this link",
     contexts: ["link"]
 });
 
-browser.contextMenus.onClicked.addListener((info, tab) => {
+chrome.contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId === "archivePage") {
         let url = `${archiveUrl}${encodeURIComponent(tab.url)}`;
-        browser.tabs.create({ url: url, active: false });
+        chrome.tabs.create({ url: url, active: false });
     } else if (info.menuItemId === "archiveLink") {
         let url = `${archiveUrl}${encodeURIComponent(info.linkUrl)}`;
-        browser.tabs.create({ url: url, active: false });
+        chrome.tabs.create({ url: url, active: false });
     }
 });
